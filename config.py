ccfd3bd167f27c5130ad2e792c99e136ab4e5019
yann ARMAND <yarma@microsoft.com>
2024-07-15 04:47:37 +0000

    use global variable to set config file

--- config.py
+++ config.py
@@ -1,7 +1,20 @@
 import configparser
-      
+
+DEFAULT_CONFIG_FILE = "'config.ini'"
+
+global config_file
+config_file = DEFAULT_CONFIG_FILE
+
+def set_config_file(file):
+  global config_file
+  config_file = file
+
+def set_default_config():
+  global config_file
+  config_file = DEFAULT_CONFIG_FILE
+
 config = False
-def getconfig(section, key, config_file='config.ini'):
+def getconfig(section, key):
   global config
   if config == False:
     config = configparser.ConfigParser()
