b2714ec1e1941956c8f692e068240b49170b8fb4
yann ARMAND <yarma@microsoft.com>
2024-09-16 18:13:58 -0700

    prepare for GraphRAG

--- db_import/doc_model.py
+++ db_import/doc_model.py
@@ -1,5 +1,15 @@
 from lancedb.pydantic import LanceModel, Vector
 
+ModelTypes = {
+  "Co": 0,
+  "C1": 1,
+  "C2": 2,
+  "document": 3,
+  "document_chunk": 4,
+  "person": 5,
+  "tag": 6,
+}
+
 class DocModel(LanceModel):
   vector: Vector(768)
   text: str
@@ -8,3 +18,4 @@ class DocModel(LanceModel):
   import_time: float
   chunk_index: int
   nb_chunks: int
+  links: list
