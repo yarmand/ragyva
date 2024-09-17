f03e8aad4703c28774acdaa5812337dcfaf60d4a
yann ARMAND <yarma@microsoft.com>
2024-09-17 04:13:17 +0000

    switch local devcontainer to universal

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+username=$1
+
+wdir=$(pwd)
+
+ln -s /home/${username}/dotfiles ~/
+cd ~/dotfiles
+./setup.sh
+
+
+cd ${wdir}
 # python libs
-pip install -r requirements.txt
+pip install --user -r requirements.txt
 
 python3 -c 'import nltk
 nltk.download("punkt")'
