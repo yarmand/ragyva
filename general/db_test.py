13cd1cc12b495a1ade4936d9d1a2f16d22276d0a
yann ARMAND <yarma@microsoft.com>
2024-09-25 04:34:27 +0000

    db.find_or_create with tests

--- general/db_test.py
+++ general/db_test.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import unittest
-from db import get_or_create_table
+from db import get_or_create_table, get_db, find_or_create
 from config import set_config_file, getconfig
+import models
 
 class mockDB:
   created_table="no-table-created"
@@ -52,5 +53,24 @@ class Test_get_or_create_table(unittest.TestCase):
     assert db.created_table == "lapin"
     assert db.dropped_table == "lapin"
 
+class Test_find_or_create(unittest.TestCase):
+  def test_create_the_entry(self):
+    table = get_or_create_table(table_name='db_test_1', delete_table=True, schema=models.Named.to_arrow_schema())
+    e = find_or_create(table=table,where="title='lapin'",entry=models.Named(id="123", short_id="123", title='lapin'))
+    ne = models.Named.model_validate(e)
+    assert ne.id == "123"
+    assert ne.title == 'lapin'
+
+  def test_find_existing_entry(self):
+    table = get_or_create_table(table_name='db_test_1', delete_table=False, schema=models.Named.to_arrow_schema())
+    existing = models.Named(id='456', short_id="456", title="aaa")
+    table.add([existing])
+    e = find_or_create(table=table,where="id='456'",entry=models.Named(id="456", short_id="error", title='lapin'))
+    ne = models.Named.model_validate(e)
+    assert ne.id == "456"
+    assert ne.short_id == '456'
+    assert ne.title == 'aaa'
+
 if __name__ == '__main__':
   unittest.main()
+  get_db().drop_table('db_test_1', ignore_missing=True)
