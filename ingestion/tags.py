3a1b00c96a6ff404f853a8a4be703c2540fdd057
yann ARMAND <yarma@microsoft.com>
2024-09-26 04:21:57 +0000

    extract tags from headers

--- ingestion/tags.py
+++ ingestion/tags.py
@@ -3,11 +3,11 @@ import re
 import sys
 from general.models import Tag
 
-# extract part of the filename within '.' as tag
-# example:
-#    filename: "1-Projects.Reliability.summer2024.md"
-#    tags: ["1-Projects","Reliability","summer2024"]
 def extract_tags_from_filename(path):
+  """ Extract part of the filename within '.' as tag"""
+  """ example:"""
+  """    filename: 1-Projects.Reliability.summer2024.md"""
+  """    tags: ['1-Projects','Reliability','summer2024']"""
   filename = os.path.basename(path)
   name, ext = os.path.splitext(filename)
   if ext == '.md':
@@ -17,8 +17,31 @@ def extract_tags_from_filename(path):
   tags = [tag for tag in tags if tag and tag != 'md']
   return tags
 
-# extract tags from the text. Tags are any piece of text starting with `#`
 def extract_tags_from_text(text):
+  """ Extract tags from the text. Tags are any piece of text starting with `#`"""
   tags = re.findall(r'#([\w-]+)', text)
   return tags
 
+def extract_tags_from_headers(path):
+  """ Extract tags from markdown file headers"""
+  """ Headers are located at the beginning of the file, between lines as ---"""
+  """ example:"""
+  """ ---"""
+  """ abstract: some description"""
+  """ tags: #foo #bar"""
+  """ ---"""
+  tags = []
+  with open(path, 'r') as file:
+    lines = file.readlines()
+    in_header = False
+    for line in lines:
+      if line.strip() == '---':
+        if in_header:
+          break
+        else:
+          in_header = True
+      elif in_header:
+        match = re.search(r'tags:\s*(.*)', line)
+        if match:
+          tags += extract_tags_from_text(match.group(1))
+  return tags
